33eac2a471cde5ad4156196cc2b423d77604d51f
pransu <>
2025-09-09 19:09:01 +0530

    Disconnect Popup added on socket disconnection

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -195,6 +195,7 @@ public class SocketIOManager : MonoBehaviour
   {
     Debug.LogWarning("⚠️ Disconnected from server.");
     isConnected = false;
+    uiManager.DisconnectionPopup();
     ResetPingRoutine();
   } //Back2 end
   private void OnPongReceived(string data) //Back2 Start
